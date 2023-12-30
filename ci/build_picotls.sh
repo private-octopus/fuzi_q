8e80762a5e5649c8cf6bba7bd3bf91681386a341
Christian Huitema <huitema@huitema.net>
2023-12-30 09:13:18 -0800

    Get latest of picotls

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -8,7 +8,7 @@ cd ..
 # git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags https://github.com/h2o/picotls
 git clone https://github.com/h2o/picotls
 cd picotls
-git checkout -q "$COMMIT_ID"
+# git checkout -q "$COMMIT_ID"
 git submodule init
 git submodule update
 cmake $CMAKE_OPTS .
